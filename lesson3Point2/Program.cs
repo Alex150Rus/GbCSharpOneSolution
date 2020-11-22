1f1f09e307e551d41870df3e6eb332cca4fe6870
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2020-11-22 16:09:56 +0300

    lesson3 point2 ready

--- lesson3Point2/Program.cs
+++ lesson3Point2/Program.cs
@@ -8,8 +8,45 @@ namespace lesson3Point2
 {
     class Program
     {
+        /// <summary>
+        /// С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). 
+        /// Требуется подсчитать сумму всех нечётных положительных чисел. 
+        /// Сами числа и сумму вывести на экран
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            bool flag = true;
+            double sum = 0;
+            string numbers = "";
+            do
+            {
+                double key = GetNumber();
+                numbers += key + " ";
+                if (key % 2 != 0)
+                    sum += key;
+               
+                if (key == 0)
+                    flag = false;
+            } while (flag);
+            Console.WriteLine(numbers);
+            Console.WriteLine($"Сумма всех нечётных положительных чисел {sum}");
+        }
+
+        /// <summary>
+        /// Метод возвращает число, введённое пользователем
+        /// </summary>
+        /// <returns>Число, введённое пользователем</returns>
+        public static double GetNumber()
+        {
+            bool flag;
+            double result;
+            do
+            {
+                string key = Console.ReadLine();
+                flag = double.TryParse(key, out result);
+            } while (!flag);
+            return result;
         }
     }
 }
