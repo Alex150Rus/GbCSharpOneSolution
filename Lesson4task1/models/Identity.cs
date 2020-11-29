ec62425871bdac1b8baa84c26ec02da677e02974
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2020-11-29 21:19:00 +0300

    Lesson5, task1 is ready

---
+++ Lesson4task1/models/Identity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Lesson5task1
+{
+    /// <summary>
+    /// Модель для хранения логина и пароля пользователя
+    /// </summary>
+    class Identity
+    {
+        /// <summary>
+        /// поле логин
+        /// </summary>
+        string login;
+        /// <summary>
+        /// поле пароль
+        /// </summary>
+        string password;
+
+        /// <summary>
+        /// Свойство для установки или получения значения логина
+        /// </summary>
+        public string Login {
+            get { return login; }
+            set { login = value; }
+        }
+    }
+}
